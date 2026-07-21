c7335bcbe2af02253efd1e4d649c33910903e2d6
ZhongxuanWu <zhongxuanwu@pku.edu.cn>
2026-07-21 11:38:32 +0000

    Deploying to gh-pages from @ ZhongxuanWu/zhongxuanwu.github.io@65e4466f2a72217b0ff5fb31e4176a2d566fba74 🚀

--- zh/assets/js/search-data.js
+++ zh/assets/js/search-data.js
@@ -41,29 +41,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/zh/services/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "文章",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "文章",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "news-我的hippocampal-replay项目已在2025年natural-environments-tasks-and-intelligence-workshop上展示",
+        },{id: "news-我的hippocampal-replay项目已在2025年natural-environments-tasks-and-intelligence-workshop上展示",
           title: '我的hippocampal replay项目已在2025年Natural Environments, Tasks, and Intelligence Workshop上展示。',
           description: "",
           section: "新闻",},{id: "news-我的个人主页zhongxuanwu-github-io成功部署在github-pages",
